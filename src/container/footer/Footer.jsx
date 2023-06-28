00f70ea0121a321c207a73eff56d2ea9440d2dab
brunowerlang <116320584+brunowerlang@users.noreply.github.com>
2023-06-28 12:46:35 -0300

    #

---
+++ src/container/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+    </div>
+  )
+}
+
+export default F;
+import './footer.css';ooter
