d500d5ddda0be1085dc8331dd8e3740ade5548a2
brunowerlang <116320584+brunowerlang@users.noreply.github.com>
2023-06-30 11:42:52 -0300

    #

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {Article, Brand, CTA, Feature, Navbar} from './components';
 
-const App = () => {
-  return (
-    <div>
-      <h1>GPT-3</h1>
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
